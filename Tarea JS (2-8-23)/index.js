b5b18fb9e50087bb281604130fbc7506a8926572
DechazJuan <manudkno66@gmail.com>
2023-08-02 21:26:19 -0300

    punto B Terminado

---
+++ Tarea JS (2-8-23)/index.js
@@ -0,0 +1,17 @@
+console.log ("Bienvenido a la Tarea");
+console.log (" ");
+console.log ("Ejercicio B");
+console.log (" ");
+
+
+for (let index = 1; index <= 10; index++) {
+    if (index === 1) {
+        console.log("-----------------------------");
+        
+    } else if (index === 10) {
+        console.log("-----------------------------");
+    } else {
+        console.log("!                           !");
+    }
+    
+}
