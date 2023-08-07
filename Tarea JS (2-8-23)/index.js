1ccd142b845149b34f22bc0f8a84bc1ded92a64b
DechazJuan <manudkno66@gmail.com>
2023-08-07 12:45:28 -0300

    primer intento piramide

--- Tarea JS (2-8-23)/index.js
+++ Tarea JS (2-8-23)/index.js
@@ -15,3 +15,46 @@ for (let index = 1; index <= 10; index++) {
     }
     
 }
+
+console.log (" ");
+console.log ("Ejercicio A");
+console.log (" ");
+
+for (let index = 1; index <= 10; index++) {
+    if (index === 1) {
+        console.log("             *");
+        
+    } else if (index === 2) {
+        console.log("           *  * *");
+    } else if (index === 3) {
+        console.log("         *     *  *");
+    } else if (index === 4) {
+        console.log("        *       *   *");
+    } else if (index === 5) {
+        console.log("      *          *    *");
+    } else if (index === 6) {
+        console.log("     *            *     *");
+    } else if (index === 7) {
+        console.log("   *               *       *");
+    } else if (index === 8) {
+        console.log("  *                 *     *");
+    } else if (index === 9) {
+        console.log(" *                   *  *");
+    }else {
+        console.log("* * * * * * * * * * * *");
+    }
+    
+}
+
+/* ejemplo priamide
+console.log ("         *      ");
+console.log ("        * * *    ");
+console.log ("       *   *  *   ");
+console.log ("      *     *   * ");
+console.log ("     *       *    *");
+console.log ("    *         *     *");
+console.log ("   *           *      *");
+console.log ("  *             *    *");
+console.log (" *               * *");
+console.log ("* * * * * * * * * *");
+*/
