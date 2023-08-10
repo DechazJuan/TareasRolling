bd989f61ea4b57f663f27df8a14f91146ebc9f59
DechazJuan <manudkno66@gmail.com>
2023-08-10 14:19:53 -0300

    commit 10-8-23

--- Tarea JS (2-8-23)/index.js
+++ Tarea JS (2-8-23)/index.js
@@ -1,47 +1,42 @@
-console.log ("Bienvenido a la Tarea");
-console.log (" ");
-console.log ("Ejercicio B");
-console.log (" ");
-
+console.log("Bienvenido a la Tarea");
+console.log(" ");
+console.log("Ejercicio B");
+console.log(" ");
 
 for (let index = 1; index <= 10; index++) {
-    if (index === 1 || index === 10) {
-        console.log("-----------------------------");
-        
-    } else {
-        console.log("!                           !");
-    }
-    
+  if (index === 1 || index === 10) {
+    console.log("-----------------------------");
+  } else {
+    console.log("|                           |");
+  }
 }
 
-console.log (" ");
-console.log ("Ejercicio A");
-console.log (" ");
+console.log(" ");
+console.log("Ejercicio A");
+console.log(" ");
 
 for (let index = 1; index <= 10; index++) {
-    if (index === 1) {
-        console.log("             *");
-        
-    } else if (index === 2) {
-        console.log("           *  * *");
-    } else if (index === 3) {
-        console.log("         *     *  *");
-    } else if (index === 4) {
-        console.log("        *       *   *");
-    } else if (index === 5) {
-        console.log("      *          *    *");
-    } else if (index === 6) {
-        console.log("     *            *     *");
-    } else if (index === 7) {
-        console.log("   *               *       *");
-    } else if (index === 8) {
-        console.log("  *                 *     *");
-    } else if (index === 9) {
-        console.log(" *                   *  *");
-    }else {
-        console.log("* * * * * * * * * * * *");
-    }
-    
+  if (index === 1) {
+    console.log("             *");
+  } else if (index === 2) {
+    console.log("           *  * *");
+  } else if (index === 3) {
+    console.log("         *     *  *");
+  } else if (index === 4) {
+    console.log("        *       *   *");
+  } else if (index === 5) {
+    console.log("      *          *    *");
+  } else if (index === 6) {
+    console.log("     *            *     *");
+  } else if (index === 7) {
+    console.log("   *               *       *");
+  } else if (index === 8) {
+    console.log("  *                 *     *");
+  } else if (index === 9) {
+    console.log(" *                   *  *");
+  } else {
+    console.log("* * * * * * * * * * * *");
+  }
 }
 
 /* ejemplo priamide
@@ -56,4 +51,3 @@ console.log ("  *             *    *");
 console.log (" *               * *");
 console.log ("* * * * * * * * * *");
 */
-
